9a60edf8dd0ef51271259679296807fe316ea0f2
Tharuka-R <sandithrajapaksha@gmail.com>
2025-06-16 12:09:25 +0530

    Configure base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react-swc';
 // VERY IMPORTANT: Replace 'your-actual-repo-name' below
 // with the name of the GitHub repository you will create for this project.
 // For example, if your GitHub repo will be named 'twitch-stats-tracker', use that.
-const GITHUB_REPO_NAME = 'your-actual-repo-name'; // <<<<====== CHANGE THIS
+const GITHUB_REPO_NAME = 'my-twitch-app'; 
 
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '/', // Default for development
+    base: '/my-twitch-app/',
   };
 
   if (command === 'build') {
